5d4ebbcb96dd576ec31556c07640f4d483721121
GGraca <ggracase@gmail.com>
2018-11-11 17:18:36 +0000

    Added GA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-129077308-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
